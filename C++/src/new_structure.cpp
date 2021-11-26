003257f292f0148838ec24ce7c82b0fc3cd437a9
izsahara <faizsahara7@gmail.com>
2021-11-26 13:52:20 +0700

    .

--- C++/src/new_structure.cpp
+++ C++/src/new_structure.cpp
@@ -137,7 +137,7 @@ public:
 		os.write(full_bar.data() + offset, width);
 		os << " [" << std::setw(3) << static_cast<int>(100 * fraction) << "%] "
 			<< " [NRMSE = " << std::setw(3) << std::left << std::setprecision(5) << std::fixed << nrmse * 100.0 << "%] "
-			<< " [R2 = " << std::setw(3) << std::left << std::setprecision(5) << std::fixed << r2 
+			<< " [R2 = " << std::setw(3) << std::left << std::setprecision(5) << std::fixed << r2 << "]"			
 			<< std::flush;
 	}	
 };
@@ -1000,14 +1000,14 @@ void nrel(std::string output, std::string exp) {
 	TMatrix Y_train = read_data(train_path);
 	TMatrix Y_test = read_data(test_path);
 
-	Graph graph(std::make_pair(X_train, Y_train), 2);
+	Graph graph(std::make_pair(X_train, Y_train), 1);
 	for (unsigned int i = 0; i < graph.n_layers; ++i) {
 		graph.layer(static_cast<int>(i))->set_kernels(TKernel::TMatern52);
 		graph.layer(static_cast<int>(i))->fix_likelihood_variance();
 	}
 	// graph.layer(1)->remove_nodes(3);
 	SIDGP model(graph);
-	model.train(100, 10);
+	model.train(500, 10);
 	MatrixPair Z = model.predict(X_test, Y_test, 100, 300);
 	TMatrix mean = Z.first;
 	TMatrix var = Z.second;
