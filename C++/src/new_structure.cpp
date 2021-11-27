2e011a562b46e1afbc38bf30536ef5d686e675f0
izsahara <faizsahara7@gmail.com>
2021-11-27 19:27:03 +0700

    airfoil

--- C++/src/new_structure.cpp
+++ C++/src/new_structure.cpp
@@ -1091,7 +1091,7 @@ void airfoil(std::string exp) {
 		graph.layer(static_cast<int>(i))->fix_likelihood_variance();
 	}
 	SIDGP model(graph);
-	model.train(100, 10);
+	model.train(250, 10);
 
 	std::cout << "Plot" << std::endl;
 	MatrixPair Zplot = model.predict(X_plot, 100, 192);
@@ -1117,7 +1117,7 @@ int main() {
 	//	nrel(output, std::to_string(i));
 	//}
 
-	for (unsigned int i = 3; i < 7; ++i) {
+	for (unsigned int i = 1; i < 7; ++i) {
 		std::cout << "================= " << " EXP " << i << " ================" << std::endl;
 		airfoil(std::to_string(i));
 	}
