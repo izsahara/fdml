a06f68f5348224036f75889bdaae929d04bc67a6
izsahara <faizsahara7@gmail.com>
2021-12-04 15:53:11 +0700

    .

--- C++/src/new_structure.cpp
+++ C++/src/new_structure.cpp
@@ -1042,11 +1042,11 @@ void nrel(std::string output, std::string exp) {
 	// write_data(ret_path, resc_true);	
 }
 void airfoil(std::string exp, bool& restart) {
-	TMatrix X_train = read_data("../datasets/airfoil/20/Xsc_train.dat");
-	TMatrix X_test = read_data("../datasets/airfoil/20/Xsc_test.dat");
-	TMatrix X_plot = read_data("../datasets/airfoil/20/X_plot.dat");
-	TMatrix Y_train = read_data("../datasets/airfoil/20/Y_train.dat");
-	TMatrix Y_test = read_data("../datasets/airfoil/20/Y_test.dat");
+	TMatrix X_train = read_data("../datasets/airfoil/20-2/Xsc_train.dat");
+	TMatrix X_test = read_data("../datasets/airfoil/20-2/Xsc_test.dat");
+	TMatrix X_plot = read_data("../datasets/airfoil/20-2/X_plot.dat");
+	TMatrix Y_train = read_data("../datasets/airfoil/20-2/Y_train.dat");
+	TMatrix Y_test = read_data("../datasets/airfoil/20-2/Y_test.dat");
 
 	Graph graph(std::make_pair(X_train, Y_train), 2);
 	for (unsigned int i = 0; i < graph.n_layers; ++i) {
@@ -1067,11 +1067,11 @@ void airfoil(std::string exp, bool& restart) {
 		restart = true;
 	}
 	else {
-		std::string e_path = "../results/airfoil/20/NRMSE.dat";
+		std::string e_path = "../results/airfoil/20-2/NRMSE.dat";
 		std::cout << "NRMSE = " << nrmse << std::endl;
 
-		std::string m_path = "../results/airfoil/20/" + exp + "-M.dat";
-		std::string v_path = "../results/airfoil/20/" + exp + "-V.dat";
+		std::string m_path = "../results/airfoil/20-2/" + exp + "-M.dat";
+		std::string v_path = "../results/airfoil/20-2/" + exp + "-V.dat";
 		write_data(m_path, mean);
 		write_data(v_path, var);
 		if (exp != "1") {
@@ -1080,7 +1080,7 @@ void airfoil(std::string exp, bool& restart) {
 			if (nrmse < min) {
 				std::cout << "Plot" << std::endl;
 				MatrixPair Zplot = model.predict(X_plot, 100, 96);
-				std::string p_path = "../results/airfoil/20/" + exp + "-P.dat";
+				std::string p_path = "../results/airfoil/20-2/" + exp + "-P.dat";
 				TMatrix Zp = Zplot.first;
 				write_data(p_path, Zp);
 			}
