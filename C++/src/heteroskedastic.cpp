bed8611dc4d69124999903e6cae550b00cd98f5e
izsahara <faizsahara7@gmail.com>
2021-12-17 11:40:37 +0700

    .

--- C++/src/heteroskedastic.cpp
+++ C++/src/heteroskedastic.cpp
@@ -1604,7 +1604,7 @@ void motorcycle_case3() {
 		study.train_iter = 500;
 		study.train_impute = 900;
 		study.pred_iter = 200;
-		study.likelihood_variance = 1E-10;
+		study.likelihood_variance = 1E-3;
 		study.plot = true;
 		case3(study);
 		//debug_case3(study);
